95ec40a9ad22962bf15656d256b1cb260cc8a284
Konstantin Voronin <heavensmiting@gmail.com>
2025-05-10 01:36:54 +0300

    дописаны новые методы, геттеры и сеттеры для классов продукт и категория

--- src/Category.py
+++ src/Category.py
@@ -1,3 +1,4 @@
+from src.Product import Product
 
 
 class Category():
@@ -11,7 +12,25 @@ class Category():
 
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
 
         Category.category_count += 1
         Category.product_count += len(self.products)
+
+    def add_product(self, product: Product) -> None:
+        """Метод для добавления товаров в категорию"""
+
+        if not isinstance(product, Product):
+            raise f"Можно добавлять только объекты от класса Product"
+
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        """Возвращает строковое представление товаров"""
+
+        return "\n".join(
+            f"{product.name}, {product.price} руб. , Остаток: {product.quantity} шт."
+                         for product in self.__products
+                         )
--- src/Product.py
+++ src/Product.py
@@ -8,5 +8,48 @@ class Product():
 
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+
+    @classmethod
+    def new_product(cls, product_data: dict, products_list: list = None):
+        """Принимает на вход параметры товара в словаре и возвращать созданный объект класса"""
+
+        for product in products_list:
+            if product.name.lower() == product_data['name'].lower():
+                product.quantity += product_data['quantity']
+                product.price = max(product.price, product_data['price'])
+                return product
+
+        return cls(
+            name=product_data['name'],
+            description=product_data['description'],
+            price=product_data['price'],
+            quantity=product_data['quantity']
+        )
+
+
+    @property
+    def price(self) -> float:
+        """Getter возвращает значение приватного атрибута цены"""
+
+        return self.__price
+
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        """Setter проверяет: в случае если цена равна или ниже нуля, выводите сообщение в консоль"""
+
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+
+        if hasattr(self, '_Product__price') and new_price < self.__price:
+            user_choice = input(
+                f"Вы действительно хотите понизить цену с {self.__price} до {new_price}? (y/n): ").lower()
+            if user_choice != 'y':
+                print("Изменение цены отменено")
+                return
+
+            self.__price = new_price
